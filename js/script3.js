e467ef7a97066075510930dce4804e9b5470f7e5
Hasina Akhter <hasinaakhter43@gmail.com>
2018-10-05 15:36:02 -0400

    fixing bugs and improving accessibility

--- js/script3.js
+++ js/script3.js
@@ -4,3 +4,6 @@ $(document).ready(function(){
     
   });
 });
+//update copyright info
+const copy = document.querySelector('.copyright');
+copy.innerHTML = '&copy; www.agoodlifeguide.hasinaakhter.com 2017-2018 All rights reserved.';
